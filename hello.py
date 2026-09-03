067c5683697bbda8f3b672d7486dbaf82417c71a
AjinkyaDhongade <ajinkyadhongade007@gmail.com>
2026-09-03 15:15:56 +0530

    Update Docker image version to v3.0 and modify version message in hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def main():
     print("Hello, World!")
-    print("This is a simple Python application running on Google Cloud Platform.")
+    print("This is a simple Python application running on Google Cloud Platform with Version 3.0.")
 
 if __name__ == "__main__":
     main()
